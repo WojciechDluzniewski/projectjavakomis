84ad647430924df2d06ed960b5a03183c3d551f5
WojciechDluzniewski <wkdluznievsky@gmail.com>
2023-01-31 21:52:56 +0100

    new project

---
+++ src/test/java/com/example/projektjavakomis/ProjektjavausedcardealerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektjavakomis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektjavausedcardealerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
